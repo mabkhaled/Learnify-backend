e126b2f27c5f22e83967a946bb2237039a78db1a
mabkhaled <mabrouk.khaled@esprit.tn>
2022-04-06 01:37:05 +0100

    checking for user info in responce

--- src/main/java/com/khaled/Learnify/controllers/AuthController.java
+++ src/main/java/com/khaled/Learnify/controllers/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -131,5 +132,19 @@ public class AuthController {
     userRepository.save(user);
 
     return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+  }
+  
+  @GetMapping("/getauthuser")
+  public ResponseEntity<?> getAuthUser(){
+	  UserDetailsImpl userDetails =
+				(UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	    List<String> roles = userDetails.getAuthorities().stream()
+	            .map(item -> item.getAuthority())
+	            .collect(Collectors.toList());
+	    return ResponseEntity.ok().body(new UserInfoResponse(userDetails.getId(),
+	                                       userDetails.getUsername(),
+	                                       userDetails.getEmail(),
+	                                       roles));
+	  
   }
 }
